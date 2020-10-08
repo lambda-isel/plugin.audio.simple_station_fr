cb20baefe6e9e72e01f732deb3c4a796fad17ab5
lambda-isel <72376459+lambda-isel@users.noreply.github.com>
2020-10-09 01:04:45 +0200

    add FIP and FranceInfo

--- addon.py
+++ addon.py
@@ -1,7 +1,9 @@
 from simple_station import SimpleStation
+from simple_station import FIP
 from simple_station import FranceInter
 from simple_station import FranceCulture
 from simple_station import FranceMusique
+from simple_station import FranceInfo
 
 if __name__ == '__main__':
   SimpleStation().run()
