83df28d502c9ae7c887c4c710df6d619e12f4c76
lambda-isel <72376459+lambda-isel@users.noreply.github.com>
2020-10-08 23:24:53 +0200

    version 0.0.1

---
+++ addon.py
@@ -0,0 +1,7 @@
+from simple_station import SimpleStation
+from simple_station import FranceInter
+from simple_station import FranceCulture
+from simple_station import FranceMusique
+
+if __name__ == '__main__':
+  SimpleStation().run()
